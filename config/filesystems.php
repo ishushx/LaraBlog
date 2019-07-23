5d2fb437ae136cbf922e609395a9c391f5fd242b
ishushx <ishushx@gmail.com>
2019-07-23 03:32:31 +0000

    add_admin_file_systems

--- config/filesystems.php
+++ config/filesystems.php
@@ -48,6 +48,11 @@ return [
             'root' => storage_path('app'),
         ],
 
+        'admin' => [
+            'driver' => 'local',
+            'root' => storage_path('app'),
+        ],
+
         'public' => [
             'driver' => 'local',
             'root' => storage_path('app/public'),
